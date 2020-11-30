789a03e8188f465d3caa16517bfaf6a24b5ae43a
YoonSung <opqudk@gmail.com>
2020-11-30 11:43:52 +0900

    다까묵었다.. 다시 시작..

--- Chapter01/ES6/02.ES6Classes.js
+++ Chapter01/ES6/02.ES6Classes.js
@@ -4,7 +4,7 @@ class User {
         this.interests = interests;
     }
     greeting () {
-        console.log('Hi, I\'m ' + this.name + '.');
+        console.log('저는' + this.name + '.');
     }
     get interestsCount () {
         return this.interests ? this.interests.length : 0;
@@ -15,19 +15,19 @@ class TeamMember extends User {
     constructor(name, interests) {
         super(name, interests);
         this._tasks = [];
-        this._welcomeText = 'Welcome to the team!';
+        this._welcomeText = '우리팀에 어서왕!';
     }
     greeting () {
-        console.log('I\' m ' + this.name + '. ' + this._welcomeText);
+        console.log('저는 ' + this.name + '. ' + this._welcomeText);
     }
     work () {
-        console.log('I\' m working on ' + this._tasks.length + ' tasks.')
+        console.log('저는' + this._tasks.length + ' 개의 일을 하고 있어요.')
     }
     set tasks (tasks) {
         let acceptedTasks = [];
         if (tasks.length > TeamMember.maxTasksCapacity()) {
             acceptedTasks = tasks.slice(0, TeamMember.maxTasksCapacity());
-            console.log('It\'s over max capacity. Can only take two.');
+            console.log('삐빅 두 개까지만 가능합니다.');
         } else {
             acceptedTasks = tasks;
         }
@@ -38,27 +38,27 @@ class TeamMember extends User {
     }
 }
 
-let member = new TeamMember('Sunny', ['Traveling']);
-member.greeting();   // I' m Sunny. Welcome to the team!
-member.tasks = ['Buy three tickets', 'Book a hotel', 'Rent a car'];
-// It's over max capacity. Can only take two.
-member.work();       // I' m working on 2 tasks.
+let member = new TeamMember('윤성', ['여행']);
+member.greeting();   // 저는 윤성. 우리팀에 어서왕!
+member.tasks = ['티캣3개 사기', '호텔 예약', '렌트카 빌리기'];
+// 삐빅 두 개까지만 가능합니다.
+member.work();       // 저는 2개의 일을 하고있어요
 console.log(member.interestsCount); // 1
 member.interestsCount = 2;          // This won’t save the change
 console.log(member.interestsCount); // 1
 
 
 User.prototype.eat = function () {
-    console.log('What will I have for lunch?');
+    console.log('점심을 뭘먹을까유?');
 };
-member.eat();  // What will I have for lunch?
+member.eat();  // 점심을 뭘먹을까유?
 
 
 User.sleep = function () {
-    console.log('Go to sleep');
+    console.log('자라');
 };
 member.sleep();  // Uncaught TypeError: member.sleep is not a function
-User.sleep();    // Go to sleep
+User.sleep();    // 자라
 
-console.log(User.prototype.hasOwnProperty('eat'));  // true
-console.log(User.hasOwnProperty('eat'));            // true
+console.log(User.prototype.hasOwnProperty('머겅'));  // true
+console.log(User.hasOwnProperty('머겅'));            // true
