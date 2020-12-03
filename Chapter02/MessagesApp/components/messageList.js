778dde6beb5555a40667c9199ae09510da416746
YoonSung <opqudk@gmail.com>
2020-12-03 11:14:21 +0900

    플러그인

--- Chapter02/MessagesApp/components/messageList.js
+++ Chapter02/MessagesApp/components/messageList.js
@@ -1,8 +1,6 @@
-import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
 import MessageListItem from './messageListItem.js';
 export default {
     name: 'MessageList',
-    mixins: [lifecycleLogger],
     template: `<ul>
                     <message-list-item 
                         v-for="item in items" 
--- Chapter02/MessagesApp/components/messageListItem.js
+++ Chapter02/MessagesApp/components/messageListItem.js
@@ -1,7 +1,5 @@
-import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
 export default {
     name: 'MessageListItem',
-    mixins: [lifecycleLogger],
     template: `<li> {{ item.text }} - {{ item.createdAt | datetime }}
     <button @click="deleteClicked">X</button></li>`,
     props: {
