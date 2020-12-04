86cd0e24710c5e6f6c427b5098e6ed59fba6e0d1
YoonSung <opqudk@gmail.com>
2020-12-04 18:10:25 +0900

    AOP Pointcuts

--- Chapter03/src/main/java/app/messages/MessageService.java
+++ Chapter03/src/main/java/app/messages/MessageService.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageService {
 
-    @Autowired
     private MessageRepository repository;
 
-/*    public MessageService (MessageRepository repository) {
-        this.repository = repository;
-    }*/
+    public MessageService (MessageRepository repository) { this.repository = repository; }
 
     public Message save(String text) {
         return repository.saveMessage(new Message(text));
