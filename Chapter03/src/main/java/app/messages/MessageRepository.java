7bc0f89c919a2530b1ed3cf92f64a2487f013eed
YoonSung <opqudk@gmail.com>
2020-12-04 14:32:53 +0900

    jdbc 드라이버

--- Chapter03/src/main/java/app/messages/MessageRepository.java
+++ Chapter03/src/main/java/app/messages/MessageRepository.java
@@ -2,15 +2,61 @@ package app.messages;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.*;
 
 @Component
 public class MessageRepository {
 
-    private final static Log log = LogFactory.getLog(MessageRepository.class);
+    private final static Log logger = LogFactory.getLog(MessageRepository.class);
 
-    public void saveMessage(Message message) {
-        // 데이터베이스에 메시지를 저장한다.
-        log.info("Saved message: " + message.getText());
+    private DataSource dataSource;
+
+    public MessageRepository(DataSource dataSource) { this.dataSource = dataSource; }
+
+    public Message saveMessage(Message message) {
+        Connection c = DataSourceUtils.getConnection(dataSource);
+
+        try {
+            String insertSql = "INSERT INTO messages (`id`, `text`, `created_date`) VALUES (null, ?, ?)";
+
+            PreparedStatement ps = c.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+
+            // SQL을 위한 파라미터
+            ps.setString(1,message.getText());
+            ps.setTimestamp(2, new Timestamp(message.getCreatedDate().getTime()));
+
+            int rowsAffected = ps.executeUpdate();
+
+            if ( rowsAffected > 0 ) {
+                // 새로 적제된 메세지 id
+                ResultSet result = ps.getGeneratedKeys();
+
+                if ( result.next() ) {
+                    int id = result.getInt(1);
+                    return new Message(id, message.getText(), message.getCreatedDate());
+                } else {
+                    logger.error("result set이 없습니다..");
+                    return null;
+                }
+            } else {
+                // 데이터 적제 실패
+                return null;
+            }
+        } catch (SQLException e) {
+            logger.error("저장에 실패했습니다.", e);
+
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                logger.error("디비연결 종료 실패");
+            }
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+        }
+        return null;
     }
 }
