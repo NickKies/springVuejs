76cb5e7c3f68598bc872b45a495fc983faed3076
YoonSung <opqudk@gmail.com>
2020-12-04 10:40:21 +0900

    타임리프

--- Chapter03/src/main/java/app/messages/MessageController.java
+++ Chapter03/src/main/java/app/messages/MessageController.java
@@ -1,17 +1,25 @@
 package app.messages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
     /*@RequestMapping(value = "/welecome", method = RequestMethod.GET)*/
     @GetMapping("/welcome") // GetMapping으로 사용해도 된다.
-    @ResponseBody
-    public String welcome() {
-        return "안녕 스프링부트야!";
+    public String welcome(Model model) {
+        model.addAttribute("message", "안녕 스프링부트야!");
+        return "welcome";
     }
+
+/*    @GetMapping("/welcome")
+    public ModelAndView welecome() {
+        ModelAndView mv = new ModelAndView("welcome");
+        mv.addObject("message", "안녕 스프링부트야!");
+        return mv;
+    }*/
 }
