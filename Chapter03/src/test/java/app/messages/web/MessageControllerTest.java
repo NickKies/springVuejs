f7fb85748b1b0d3a44f257304852f5ea30b32afd
YoonSung <opqudk@gmail.com>
2020-12-09 11:05:19 +0900

    test controller

---
+++ Chapter03/src/test/java/app/messages/web/MessageControllerTest.java
@@ -0,0 +1,48 @@
+package app.messages.web;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import app.messages.model.Message;
+import app.messages.service.MessageService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MessageController.class)
+public class MessageControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @MockBean
+  private MessageService service;
+
+  @Test
+  // 작업_단위테스트 중인 상태_예상되는 행동
+  //public void getMessages_existingMessages_shouldReturnJsonArray() throws Exception {
+  public void 메세지가_제이슨형식으로_리턴된다() throws Exception {
+    Message firstMessage = new Message("첫번째 메세지");
+    List<Message> allMessages = Arrays.asList(firstMessage);
+
+    when(service.getMessage()).thenReturn(allMessages);
+
+    mvc.perform(get("/api/messages").contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$", hasSize(1)))
+      .andExpect(jsonPath("$[0].text", is(firstMessage.getText())));
+  }
+}
